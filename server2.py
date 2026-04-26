fa2b55da63b2517c1cbf67a43db5afc139080a84
Isabella Oliveira <138396545+isabella3107@users.noreply.github.com>
2026-04-26 15:19:44 -0300

    Update server2.py
    Refactor Printer class implementations and add new methods.

--- server2.py
+++ server2.py
@@ -1,20 +1,45 @@
 import sys, Ice
 import Demo
- 
-class PrinterI(Demo.Printer):
+
+class Printer1I(Demo.Printer):
     def __init__(self, t):
         self.t = t
-        
+
     def printString(self, s, current=None):
         print(self.t, s)
 
-communicator = Ice.initialize(sys.argv) 
+    def toUpper(self, s, current=None):
+        return s.upper()
 
-adapter = communicator.createObjectAdapterWithEndpoints("SimpleAdapter", "default -p 11000")
-object1 = PrinterI("Object1 says:")
-object2 = PrinterI("Object2 says:")
-adapter.add(object1, communicator.stringToIdentity("SimplePrinter1"))
-adapter.add(object2, communicator.stringToIdentity("SimplePrinter2"))
-adapter.activate()
+    def concat(self, a, b, current=None):
+        return a + b
 
-communicator.waitForShutdown()
+
+class Printer2I(Demo.Printer):
+    def __init__(self, t):
+        self.t = t
+
+    def printString(self, s, current=None):
+        print(self.t, s)
+
+    def toUpper(self, s, current=None):
+        return "[P2] " + s.upper()
+
+    def concat(self, a, b, current=None):
+        return "[P2] " + a + b
+
+
+with Ice.initialize(sys.argv) as communicator:
+
+    adapter = communicator.createObjectAdapterWithEndpoints(
+        "SimpleAdapter", "default -p 11000"
+    )
+
+    object1 = Printer1I("Object1 says:")
+    object2 = Printer2I("Object2 says:")
+
+    adapter.add(object1, communicator.stringToIdentity("SimplePrinter1"))
+    adapter.add(object2, communicator.stringToIdentity("SimplePrinter2"))
+
+    adapter.activate()
+    communicator.waitForShutdown()
