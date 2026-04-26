78fdbdb9a22ece8814a0438cfec10b7ceedc0179
Isabella Oliveira <138396545+isabella3107@users.noreply.github.com>
2026-04-26 15:20:39 -0300

    Update client2.py
    Updated print statements and added calls to 'toUpper' and 'concat' methods.

--- client2.py
+++ client2.py
@@ -10,7 +10,13 @@ printer2 = Demo.PrinterPrx.checkedCast(base2)
 if (not printer1) or (not printer2):
     raise RuntimeError("Invalid proxy")
 
-printer1.printString("Hello World from printer1!")
-printer2.printString("Hello World from printer2!")
+printer1.printString("Hello from printer1")
+printer2.printString("Hello from printer2")
+
+print(printer1.toUpper("teste"))
+print(printer2.toUpper("teste"))
+
+print(printer1.concat("A", "B"))
+print(printer2.concat("A", "B"))
 
 communicator.waitForShutdown()
